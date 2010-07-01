fc8705cb9378efbdf8160f9c8fdddf246d4127ad
Robbie Hanson <robbiehanson@deusty.com>
2010-07-01 18:00:48 +0000

    Bug fix for issue #1 - Now supports universal apps that run on both iOS 4 (with GCD) and prior OS versions (without GCD).

---
+++ Xcode/UniversalApp/Classes/UniversalAppViewController.h
@@ -0,0 +1,16 @@
+//
+//  UniversalAppViewController.h
+//  UniversalApp
+//
+//  Created by Robbie Hanson on 7/1/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UniversalAppViewController : UIViewController {
+
+}
+
+@end
+
