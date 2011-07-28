2f0f51f7067bb7fe2e5f71122a78757f4c4dee08
Robbie Hanson <robbiehanson@deusty.com>
2011-07-28 11:38:23 -0700

    Working on a few ideas : 1) Using a config file for log levels. 2) Different log levels for different developers.

---
+++ Xcode/LogLevelsConfigFile/LogLevelsConfigFile/Lions.m
@@ -0,0 +1,27 @@
+#import "Lions.h"
+#import "DDLogLevelsConfig.h"
+#import "DDLog.h"
+
+static int ddLogLevel;
+
+
+@implementation Lions
+
++ (void)initialize
+{
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		
+		ddLogLevel = [[DDLogLevelsConfig config:@"DebugLogging.txt"] levelFor:THIS_FILE];
+	});
+}
+
++ (void)printLogStatements
+{
+	DDLogError(@"%@ - Error", THIS_FILE);
+	DDLogWarn(@"%@ - Warn", THIS_FILE);
+	DDLogInfo(@"%@ - Info", THIS_FILE);
+	DDLogVerbose(@"%@ - Verbose", THIS_FILE);
+}
+
+@end
