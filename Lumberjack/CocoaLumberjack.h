ba18132cf884e7af811285bfb006a7a321687524
oleksandr dodatko <dodikk88.reg@gmail.com>
2011-10-18 11:10:38 +0300

    Added a main header file

---
+++ Lumberjack/CocoaLumberjack.h
@@ -0,0 +1,10 @@
+#ifndef __COCOA_LUMBERJACK_MAIN_HEADER_H__
+#define __COCOA_LUMBERJACK_MAIN_HEADER_H__
+
+#import <CocoaLumberjack/DDASLLogger.h>
+#import <CocoaLumberjack/DDAbstractDatabaseLogger.h>
+#import <CocoaLumberjack/DDFileLogger.h>
+#import <CocoaLumberjack/DDLog.h>
+#import <CocoaLumberjack/DDTTYLogger.h>
+
+#endif //__COCOA_LUMBERJACK_MAIN_HEADER_H__
