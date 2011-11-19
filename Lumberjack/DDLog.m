20172dc4f7ff25a56b0c66134ff3a2822e1c69fe
Robbie Hanson <robbiehanson@deusty.com>
2011-11-18 17:44:25 -0800

    Changing the language used in warning directive. Making it a bit… nicer.

--- Lumberjack/DDLog.m
+++ Lumberjack/DDLog.m
@@ -19,7 +19,7 @@
 **/
 
 #if ! __has_feature(objc_arc)
-#warning This file must be compiled with ARC. Either turn on ARC for the project or use -fobjc-arc flag.
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
 #endif
 
 // We probably shouldn't be using DDLog() statements within the DDLog implementation.
--- Lumberjack/Extensions/ContextFilterLogFormatter.m
+++ Lumberjack/Extensions/ContextFilterLogFormatter.m
@@ -12,7 +12,7 @@
 **/
 
 #if ! __has_feature(objc_arc)
-#warning This file must be compiled with ARC. Either turn on ARC for the project or use -fobjc-arc flag.
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
 #endif
 
 @interface LoggingContextSet : NSObject
