4d9f1b82520538e875e4d6408d0b217d9c5dc15c
Robbie Hanson <robbiehanson@deusty.com>
2012-02-25 14:32:52 -0800

    Fixes issue #17

--- Lumberjack/DDFileLogger.h
+++ Lumberjack/DDFileLogger.h
@@ -96,7 +96,7 @@
 // 
 // All log files are placed inside the logsDirectory.
 // If a specific logsDirectory isn't specified, the default directory is used.
-// On Mac, this is in ~/Library/Application Support/<Application Name>/Logs.
+// On Mac, this is in ~/Library/Logs/<Application Name>.
 // On iPhone, this is in ~/Library/Caches/Logs.
 // 
 // Log files are named "log-<uuid>.txt",
