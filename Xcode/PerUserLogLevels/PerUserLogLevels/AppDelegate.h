bdf6ade8ecbfbf865aa0cae85995b13cace21d6b
Robbie Hanson <robbiehanson@deusty.com>
2012-03-30 02:44:22 -0700

    Adding per-user log levels example!

---
+++ Xcode/PerUserLogLevels/PerUserLogLevels/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PerUserLogLevels
+//
+//  Created by Robbie Hanson on 3/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
