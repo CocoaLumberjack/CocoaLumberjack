a289677b3554d01b32855d27599d1b7b7dbc1b17
Mahesh Shanbhag <rayashanbhag@gmail.com>
2014-04-03 11:45:10 +0530

    Removed the empty +initialize method

--- Lumberjack/DDTTYLogger.m
+++ Lumberjack/DDTTYLogger.m
@@ -765,20 +765,6 @@ static DDTTYLogger *sharedInstance;
     return bestIndex;
 }
 
-/**
- * The runtime sends initialize to each class in a program exactly one time just before the class,
- * or any class that inherits from it, is sent its first message from within the program. (Thus the
- * method may never be invoked if the class is not used.) The runtime sends the initialize message to
- * classes in a thread-safe manner. Superclasses receive this message before their subclasses.
- *
- * This method may also be called directly (assumably by accident), hence the safety mechanism.
-**/
-+ (void)initialize
-{
-    // the code has been moved to shareInstance where dispatch_once_t function is used
-    // omitting the accidental call to this method.
-}
-
 + (instancetype)sharedInstance
 {
     static dispatch_once_t DDTTYLoggerOnceToken;
