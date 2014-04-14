8a60435106203d0b3bb43ab93318e7956fc085f5
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2014-04-14 18:00:58 +0900

    Add LOG_FLAG_VERBOSE to DDASLLogger and better correlate ASL levels in capture module

--- Lumberjack/DDASLLogger.m
+++ Lumberjack/DDASLLogger.m
@@ -69,12 +69,12 @@ static DDASLLogger *sharedInstance;
         {
             // Note: By default ASL will filter anything above level 5 (Notice).
             // So our mappings shouldn't go above that level.
-            
-            case LOG_FLAG_ERROR : aslLogLevel = ASL_LEVEL_ALERT;   break;
-            case LOG_FLAG_WARN  : aslLogLevel = ASL_LEVEL_CRIT;    break;
-            case LOG_FLAG_INFO  : aslLogLevel = ASL_LEVEL_ERR;     break;
-            case LOG_FLAG_DEBUG : aslLogLevel = ASL_LEVEL_WARNING; break;
-            default             : aslLogLevel = ASL_LEVEL_NOTICE;  break;
+            case LOG_FLAG_ERROR     : aslLogLevel = ASL_LEVEL_CRIT;     break;
+            case LOG_FLAG_WARN      : aslLogLevel = ASL_LEVEL_ERR;      break;
+            case LOG_FLAG_INFO      : aslLogLevel = ASL_LEVEL_WARNING;  break; // Regular NSLog's level
+            case LOG_FLAG_DEBUG     :
+            case LOG_FLAG_VERBOSE   :
+            default                 : aslLogLevel = ASL_LEVEL_NOTICE;   break;
         }
         
         aslmsg m = asl_new(ASL_TYPE_MSG);
