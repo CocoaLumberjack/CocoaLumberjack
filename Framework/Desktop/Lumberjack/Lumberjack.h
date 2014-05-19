9c7d6949c11e7c63b360c50abcc501295e6952bc
Bogdan Poplauschi <bpoplauschi@gmail.com>
2014-05-19 13:03:50 +0300

    Updated Desktop framework Xcode project (new paths)

--- Framework/Desktop/Lumberjack/Lumberjack.h
+++ Framework/Desktop/Lumberjack/Lumberjack.h
@@ -1,11 +1,11 @@
 
-#import <Lumberjack/DDLog.h>
+#import "DDLog.h"
 
-#import <Lumberjack/DDAbstractDatabaseLogger.h>
-#import <Lumberjack/DDASLLogger.h>
-#import <Lumberjack/DDFileLogger.h>
-#import <Lumberjack/DDTTYLogger.h>
+#import "DDAbstractDatabaseLogger.h"
+#import "DDASLLogger.h"
+#import "DDFileLogger.h"
+#import "DDTTYLogger.h"
 
-#import <Lumberjack/DDContextFilterLogFormatter.h>
-#import <Lumberjack/DDDispatchQueueLogFormatter.h>
-#import <Lumberjack/DDMultiFormatter.h>
+#import "DDContextFilterLogFormatter.h"
+#import "DDDispatchQueueLogFormatter.h"
+#import "DDMultiFormatter.h"
