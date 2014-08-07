80f4510ca907b2260a6ad6c5dabeb47f413ea5d4
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2014-08-07 17:51:51 +0900

    New CocoaLumberjack.h header file

--- Classes/DDLog.h
+++ Classes/DDLog.h
@@ -1,53 +1,5 @@
 #import <Foundation/Foundation.h>
 
-/**
- * Welcome to CocoaLumberjack!
- * 
- * The project page has a wealth of documentation if you have any questions.
- * https://github.com/CocoaLumberjack/CocoaLumberjack
- * 
- * If you're new to the project you may wish to read the "Getting Started" wiki.
- * https://github.com/CocoaLumberjack/CocoaLumberjack/wiki/GettingStarted
- * 
- * Otherwise, here is a quick refresher.
- * There are three steps to using the macros:
- * 
- * Step 1:
- * Import the header in your implementation file:
- * 
- * #import "DDLog.h"
- * 
- * Step 2:
- * Define your logging level in your implementation file:
- * 
- * // Log levels: off, error, warn, info, verbose
- * static const int ddLogLevel = LOG_LEVEL_VERBOSE;
- * 
- * Step 2 [3rd party frameworks]:
- *
- * Define your LOG_LEVEL_DEF to a different variable/function than ddLogLevel:
- *
- * // #undef LOG_LEVEL_DEF // Undefine first only if needed
- * #define LOG_LEVEL_DEF myLibLogLevel
- *
- * Define your logging level in your implementation file:
- *
- * // Log levels: off, error, warn, info, verbose
- * static const int myLibLogLevel = LOG_LEVEL_VERBOSE;
- *
- * Step 3:
- * Replace your NSLog statements with DDLog statements according to the severity of the message.
- * 
- * NSLog(@"Fatal error, no dohickey found!"); -> DDLogError(@"Fatal error, no dohickey found!");
- * 
- * DDLog works exactly the same as NSLog.
- * This means you can pass it multiple variables just like NSLog.
-**/
-
-#ifndef LOG_LEVEL_DEF
-    #define LOG_LEVEL_DEF ddLogLevel
-#endif
-
 @class DDLogMessage;
 
 @protocol DDLogger;
@@ -227,12 +179,6 @@
 #define LOG_ASYNC_INFO     (YES && LOG_ASYNC_ENABLED)
 #define LOG_ASYNC_DEBUG    (YES && LOG_ASYNC_ENABLED)
 #define LOG_ASYNC_VERBOSE  (YES && LOG_ASYNC_ENABLED)
-
-#define DDLogError(frmt, ...)   LOG_OBJC_MAYBE(LOG_ASYNC_ERROR,   LOG_LEVEL_DEF, LOG_FLAG_ERROR,   0, frmt, ##__VA_ARGS__)
-#define DDLogWarn(frmt, ...)    LOG_OBJC_MAYBE(LOG_ASYNC_WARN,    LOG_LEVEL_DEF, LOG_FLAG_WARN,    0, frmt, ##__VA_ARGS__)
-#define DDLogInfo(frmt, ...)    LOG_OBJC_MAYBE(LOG_ASYNC_INFO,    LOG_LEVEL_DEF, LOG_FLAG_INFO,    0, frmt, ##__VA_ARGS__)
-#define DDLogDebug(frmt, ...)   LOG_OBJC_MAYBE(LOG_ASYNC_DEBUG,   LOG_LEVEL_DEF, LOG_FLAG_DEBUG,   0, frmt, ##__VA_ARGS__)
-#define DDLogVerbose(frmt, ...) LOG_OBJC_MAYBE(LOG_ASYNC_VERBOSE, LOG_LEVEL_DEF, LOG_FLAG_VERBOSE, 0, frmt, ##__VA_ARGS__)
 
 /**
  * The THIS_FILE macro gives you an NSString of the file name.
