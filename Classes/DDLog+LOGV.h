6e27f2038e28f1eb95463bc2ea94065a867a199a
Bogdan Poplauschi <bpoplauschi@gmail.com>
2014-08-12 16:23:13 +0300

    Updated formatting

--- Classes/DDLog+LOGV.h
+++ Classes/DDLog+LOGV.h
@@ -24,24 +24,24 @@
           args : avalist]
 
 #define LOGV_OBJC_MACRO(async, lvl, flg, ctx, frmt, avalist) \
-    LOGV_MACRO(async, lvl, flg, ctx, nil, sel_getName(_cmd), frmt, avalist)
+             LOGV_MACRO(async, lvl, flg, ctx, nil, sel_getName(_cmd), frmt, avalist)
 
 #define LOGV_C_MACRO(async, lvl, flg, ctx, frmt, avalist) \
-    LOGV_MACRO(async, lvl, flg, ctx, nil, __FUNCTION__, frmt, avalist)
+          LOGV_MACRO(async, lvl, flg, ctx, nil, __FUNCTION__, frmt, avalist)
 
 
 
 #define  SYNC_LOGV_OBJC_MACRO(lvl, flg, ctx, frmt, avalist) \
-    LOGV_OBJC_MACRO(NO, lvl, flg, ctx, frmt, avalist)
+          LOGV_OBJC_MACRO(NO, lvl, flg, ctx, frmt, avalist)
 
 #define ASYNC_LOGV_OBJC_MACRO(lvl, flg, ctx, frmt, avalist) \
-    LOGV_OBJC_MACRO(YES, lvl, flg, ctx, frmt, avalist)
+         LOGV_OBJC_MACRO(YES, lvl, flg, ctx, frmt, avalist)
 
 #define  SYNC_LOGV_C_MACRO(lvl, flg, ctx, frmt, avalist) \
-    LOGV_C_MACRO(NO, lvl, flg, ctx, frmt, avalist)
+          LOGV_C_MACRO(NO, lvl, flg, ctx, frmt, avalist)
 
 #define ASYNC_LOGV_C_MACRO(lvl, flg, ctx, frmt, avalist) \
-    LOGV_C_MACRO(YES, lvl, flg, ctx, frmt, avalist)
+         LOGV_C_MACRO(YES, lvl, flg, ctx, frmt, avalist)
 
 
 
@@ -50,39 +50,39 @@
 
 
 #define LOGV_OBJC_MAYBE(async, lvl, flg, ctx, frmt, avalist) \
-    LOGV_MAYBE(async, lvl, flg, ctx, sel_getName(_cmd), frmt, avalist)
+             LOGV_MAYBE(async, lvl, flg, ctx, sel_getName(_cmd), frmt, avalist)
 
 #define LOGV_C_MAYBE(async, lvl, flg, ctx, frmt, avalist) \
-    LOGV_MAYBE(async, lvl, flg, ctx, __FUNCTION__, frmt, avalist)
+          LOGV_MAYBE(async, lvl, flg, ctx, __FUNCTION__, frmt, avalist)
 
 #define  SYNC_LOGV_OBJC_MAYBE(lvl, flg, ctx, frmt, avalist) \
-    LOGV_OBJC_MAYBE(NO, lvl, flg, ctx, frmt, avalist)
+          LOGV_OBJC_MAYBE(NO, lvl, flg, ctx, frmt, avalist)
 
 #define ASYNC_LOGV_OBJC_MAYBE(lvl, flg, ctx, frmt, avalist) \
-    LOGV_OBJC_MAYBE(YES, lvl, flg, ctx, frmt, avalist)
+         LOGV_OBJC_MAYBE(YES, lvl, flg, ctx, frmt, avalist)
 
 #define  SYNC_LOGV_C_MAYBE(lvl, flg, ctx, frmt, avalist) \
-    LOGV_C_MAYBE(NO, lvl, flg, ctx, frmt, avalist)
+          LOGV_C_MAYBE(NO, lvl, flg, ctx, frmt, avalist)
 
 #define ASYNC_LOGV_C_MAYBE(lvl, flg, ctx, frmt, avalist) \
-    LOGV_C_MAYBE(YES, lvl, flg, ctx, frmt, avalist)
+         LOGV_C_MAYBE(YES, lvl, flg, ctx, frmt, avalist)
 
 
 
 #define LOGV_OBJC_TAG_MACRO(async, lvl, flg, ctx, tag, frmt, avalist) \
-    LOGV_MACRO(async, lvl, flg, ctx, tag, sel_getName(_cmd), frmt, avalist)
+                 LOGV_MACRO(async, lvl, flg, ctx, tag, sel_getName(_cmd), frmt, avalist)
 
 #define LOGV_C_TAG_MACRO(async, lvl, flg, ctx, tag, frmt, avalist) \
-    LOGV_MACRO(async, lvl, flg, ctx, tag, __FUNCTION__, frmt, avalist)
+              LOGV_MACRO(async, lvl, flg, ctx, tag, __FUNCTION__, frmt, avalist)
 
 #define LOGV_TAG_MAYBE(async, lvl, flg, ctx, tag, fnct, frmt, avalist) \
     do { if(lvl & flg) LOGV_MACRO(async, lvl, flg, ctx, tag, fnct, frmt, avalist); } while(0)
 
 #define LOGV_OBJC_TAG_MAYBE(async, lvl, flg, ctx, tag, frmt, avalist) \
-    LOGV_TAG_MAYBE(async, lvl, flg, ctx, tag, sel_getName(_cmd), frmt, avalist)
+             LOGV_TAG_MAYBE(async, lvl, flg, ctx, tag, sel_getName(_cmd), frmt, avalist)
 
 #define LOGV_C_TAG_MAYBE(async, lvl, flg, ctx, tag, frmt, avalist) \
-    LOGV_TAG_MAYBE(async, lvl, flg, ctx, tag, __FUNCTION__, frmt, avalist)
+          LOGV_TAG_MAYBE(async, lvl, flg, ctx, tag, __FUNCTION__, frmt, avalist)
 
 
 
