77103063f9c4ecf0677affd285913b79f340fb7f
Ernesto Rivera <rivera_ernesto@cyberagent.co.jp>
2014-12-19 11:16:40 +0900

    Reword warning message

--- Classes/DDLegacy.h
+++ Classes/DDLegacy.h
@@ -18,7 +18,7 @@
  **/
 
 #warning CocoaLumberjack 1.9.x legacy macros enabled. \
-Prefer importing CocoaLumberjack.h or DDLogMacros.h instead of DDLog.h or add `#define DD_LEGACY_MACROS 0` before importing DDLog.h.
+Disable legacy macros by importing CocoaLumberjack.h or DDLogMacros.h instead of DDLog.h or add `#define DD_LEGACY_MACROS 0` before importing DDLog.h.
 
 #ifndef LOG_LEVEL_DEF
     #define LOG_LEVEL_DEF ddLogLevel
