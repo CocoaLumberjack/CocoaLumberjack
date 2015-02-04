78e2fbf97e684fb9c74988f7ba2aa4ca67a80c40
Andrew Mackenzie-Ross <andrew@mackross.net>
2015-02-03 19:53:42 -0800

    Simplify framework and add Carthage support.

---
+++ Framework/CocoaLumberjack-iOS-Static/CocoaLumberjack_iOS_Static.h
@@ -0,0 +1,13 @@
+//
+//  CocoaLumberjack_iOS_Static.h
+//  CocoaLumberjack-iOS-Static
+//
+//  Created by Andrew Mackenzie-Ross on 3/02/2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaLumberjack_iOS_Static : NSObject
+
+@end
---
+++ Framework/iOSLibStaticTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSLibStaticTest
+//
+//  Created by Andrew Mackenzie-Ross on 3/02/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
