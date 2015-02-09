9b9f089cf07f425ca2dc28a8ae977f51647f40cc
Bogdan Poplauschi <bpoplauschi@gmail.com>
2015-02-09 22:51:07 +0200

    Fix #433 CocoaLumberjack 2.0.0-rc does not build with Pods

--- Classes/DDLog.h
+++ Classes/DDLog.h
@@ -23,7 +23,7 @@
     #import "DDLegacyMacros.h"
 #endif
 
-#if OS_OBJECT_HAVE_OBJC_SUPPORT
+#if OS_OBJECT_HAVE_OBJC_SUPPORT || __IPHONE_OS_VERSION_MIN_REQUIRED >= 60000
     #define DISPATCH_QUEUE_REFERENCE_TYPE strong
 #else
     #define DISPATCH_QUEUE_REFERENCE_TYPE assign
