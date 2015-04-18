34cdf8256ab7444e1cf2a0dfe5b53478fe904de6
Johannes Weiß <johannes.weiss@bromium.com>
2015-04-18 13:55:33 +0100

    Issue #488: Support DDLog without AppKit Dependency (#define DD_CLI)

--- Classes/DDTTYLogger.h
+++ Classes/DDTTYLogger.h
@@ -44,15 +44,15 @@
     #import <UIKit/UIColor.h>
     typedef UIColor DDColor;
     static inline DDColor* DDMakeColor(CGFloat r, CGFloat g, CGFloat b) {return [DDColor colorWithRed:(r/255.0f) green:(g/255.0f) blue:(b/255.0f) alpha:1.0f];}
-#elif __has_include(<AppKit/NSColor.h>)
-    // OS X with AppKit
-    #import <AppKit/NSColor.h>
-    typedef NSColor DDColor;
-    static inline DDColor* DDMakeColor(CGFloat r, CGFloat g, CGFloat b) {return [DDColor colorWithCalibratedRed:(r/255.0f) green:(g/255.0f) blue:(b/255.0f) alpha:1.0f];}
-#else
+#elif defined(DD_CLI) || !__has_include(<AppKit/NSColor.h>)
     // OS X CLI
     #import "CLIColor.h"
     typedef CLIColor DDColor;
+    static inline DDColor* DDMakeColor(CGFloat r, CGFloat g, CGFloat b) {return [DDColor colorWithCalibratedRed:(r/255.0f) green:(g/255.0f) blue:(b/255.0f) alpha:1.0f];}
+#else
+    // OS X with AppKit
+    #import <AppKit/NSColor.h>
+    typedef NSColor DDColor;
     static inline DDColor* DDMakeColor(CGFloat r, CGFloat g, CGFloat b) {return [DDColor colorWithCalibratedRed:(r/255.0f) green:(g/255.0f) blue:(b/255.0f) alpha:1.0f];}
 #endif
 #pragma clang diagnostic pop
