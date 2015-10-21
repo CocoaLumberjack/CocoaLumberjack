f5ea328b3bfc2967b8432fbda32acbdffe2a3919
Jaehong Kang <sinoru@me.com>
2015-10-22 05:00:52 +0900

    Revert codes from merge

--- Classes/Extensions/DDMultiFormatter.m
+++ Classes/Extensions/DDMultiFormatter.m
@@ -23,8 +23,8 @@
 #else                                         // iOS 5.X or earlier
 #define NEEDS_DISPATCH_RETAIN_RELEASE 1
 #endif
-#elif TARGET_OS_WATCH
-// Compiling for watchOS
+#elif TARGET_OS_WATCH || TARGET_OS_TV
+// Compiling for watchOS, tvOS
 #define NEEDS_DISPATCH_RETAIN_RELEASE 0
 #else
 // Compiling for Mac OS X
