8436496b0ea31d405ea5fa1e9516e22d6aff818f
Kent Sutherland <git@ksuther.com>
2017-07-06 13:37:12 -0500

    Fixed new log files being deleted immediately when there are logs with the old date format. -[DDFileLogger sortedLogFileInfos] was assuming that all files are using the new date format introduced in 44ec1fafc3acab53513e4c0474ee58ee17b044af. This was resulting in old log files ending up with a nil date when being parsed by logFileDateFormatter, which would result in a bad compare: result. Fixed typo in variable name (arryComponant -> arrayComponent).

--- Classes/DDFileLogger.m
+++ Classes/DDFileLogger.m
@@ -388,23 +388,23 @@ unsigned long long const kDDDefaultLogFilesDiskQuota   = 20 * 1024 * 1024; // 20
 
 - (NSArray *)sortedLogFileInfos {
     return  [[self unsortedLogFileInfos] sortedArrayUsingComparator:^NSComparisonResult(DDLogFileInfo   * _Nonnull obj1, DDLogFileInfo   * _Nonnull obj2) {
-        
         NSDate *date1 = [NSDate new];
         NSDate *date2 = [NSDate new];
-        NSArray *arryComponant = [[obj1 fileName] componentsSeparatedByString:@" "];
-        if(arryComponant.count>0){
-            NSString *stringDate = arryComponant.lastObject;
+
+        NSArray<NSString *> *arrayComponent = [[obj1 fileName] componentsSeparatedByString:@" "];
+        if (arrayComponent.count > 0) {
+            NSString *stringDate = arrayComponent.lastObject;
             stringDate = [stringDate stringByReplacingOccurrencesOfString:@".log" withString:@""];
             stringDate = [stringDate stringByReplacingOccurrencesOfString:@".archived" withString:@""];
-            date1 = [[self logFileDateFormatter] dateFromString:stringDate];
+            date1 = [[self logFileDateFormatter] dateFromString:stringDate] ?: [obj1 creationDate];
         }
         
-        arryComponant = [[obj2 fileName] componentsSeparatedByString:@" "];
-        if(arryComponant.count>0){
-            NSString *stringDate = arryComponant.lastObject;
+        arrayComponent = [[obj2 fileName] componentsSeparatedByString:@" "];
+        if (arrayComponent.count > 0) {
+            NSString *stringDate = arrayComponent.lastObject;
             stringDate = [stringDate stringByReplacingOccurrencesOfString:@".log" withString:@""];
             stringDate = [stringDate stringByReplacingOccurrencesOfString:@".archived" withString:@""];
-            date2 = [[self logFileDateFormatter] dateFromString:stringDate];
+            date2 = [[self logFileDateFormatter] dateFromString:stringDate] ?: [obj2 creationDate];
         }
         
         return [date2 compare:date1];
