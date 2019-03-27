450fd60d4a2487f9a17ba74b1a1ebbabd74e3966
Stephan Diederich <stephan.diederich@gmail.com>
2019-03-27 17:05:34 +0700

    always log the assertion
    don't pass the current log-level into the assertion, we want to
    always log this

--- Classes/DDAssert.swift
+++ Classes/DDAssert.swift
@@ -24,9 +24,9 @@
  *     The default is an empty string.
  */
 @inlinable
-public func DDAssert(_ condition: @autoclosure () -> Bool, _ message: @autoclosure () -> String = "", level: DDLogLevel = DDDefaultLogLevel, context: Int = 0, file: StaticString = #file, function: StaticString = #function, line: UInt = #line, tag: Any? = nil, asynchronous async: Bool = false, ddlog: DDLog = DDLog.sharedInstance) {
+public func DDAssert(_ condition: @autoclosure () -> Bool, _ message: @autoclosure () -> String = "", context: Int = 0, file: StaticString = #file, function: StaticString = #function, line: UInt = #line, tag: Any? = nil, asynchronous async: Bool = false, ddlog: DDLog = DDLog.sharedInstance) {
     if !condition() {
-        DDLogError(message(), level: level, context: context, file: file, function: function, line: line, tag: tag, asynchronous: async, ddlog: ddlog)
+        DDLogError(message(), level: .all, context: context, file: file, function: function, line: line, tag: tag, asynchronous: async, ddlog: ddlog)
         Swift.assertionFailure(message(), file: file, line: line)
     }
 }
@@ -39,7 +39,7 @@ public func DDAssert(_ condition: @autoclosure () -> Bool, _ message: @autoclosu
  *   - message: A string to log (using `DDLogError`). The default is an empty string.
  */
 @inlinable
-public func DDAssertionFailure(_ message: @autoclosure () -> String = "", level: DDLogLevel = DDDefaultLogLevel, context: Int = 0, file: StaticString = #file, function: StaticString = #function, line: UInt = #line, tag: Any? = nil, asynchronous async: Bool = false, ddlog: DDLog = DDLog.sharedInstance) {
-    DDLogError(message(), level: level, context: context, file: file, function: function, line: line, tag: tag, asynchronous: async, ddlog: ddlog)
+public func DDAssertionFailure(_ message: @autoclosure () -> String = "", context: Int = 0, file: StaticString = #file, function: StaticString = #function, line: UInt = #line, tag: Any? = nil, asynchronous async: Bool = false, ddlog: DDLog = DDLog.sharedInstance) {
+    DDLogError(message(), level: .all, context: context, file: file, function: function, line: line, tag: tag, asynchronous: async, ddlog: ddlog)
     Swift.assertionFailure(message(), file: file, line: line)
 }
