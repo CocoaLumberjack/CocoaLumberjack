c603f4afa23c0e384d816404a33b63fa709f2164
Dmitry Lobanov <gaussblurinc@gmail.com>
2019-07-12 13:13:11 +0300

    deployment: package swift supported platforms have been specified.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "CocoaLumberjack",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .watchOS(.v3),
+        .tvOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
