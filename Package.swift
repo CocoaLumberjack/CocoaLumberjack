4d15a285e9bced3cec165dc499f53f8df733f70d
Dmitry Lobanov <gaussblurinc@gmail.com>
2019-07-16 20:25:17 +0300

    deployment: swift package has been updated after migration.

--- Package.swift
+++ Package.swift
@@ -16,33 +16,24 @@ let package = Package(
         .library(
             name: "CocoaLumberjack",
             targets: ["CocoaLumberjack",
-//                      "CocoaLumberjackSwift"
+                      "CocoaLumberjackSwift"
         ]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "CocoaLumberjack",
-                path: ".",
-                exclude: ["Classes/CocoaLumberjack.swift",
-                          "Classes/DDAssert.swift"],
-                sources: ["Classes"],
-                publicHeadersPath: "Classes/Include"),
-//        .target(name: "CocoaLumberjackSwift",
-//                dependencies: ["CocoaLumberjack"],
-//                path: ".",
-//                exclude: [],
-//                sources: ["Classes/CocoaLumberjack.swift", "Classes/DDAssert.swift"],
-//                publicHeadersPath: "Classes/Include/Swift"),
-//        .target(name: "CocoaLumberjackTestsHelper",
-//                dependencies: ["CocoaLumberjack"],
-//                path: "Tests",
-//                sources: ["Library"],
-//                publicHeadersPath: "Library"),
-        .testTarget(name: "Tests",
-                    dependencies: ["CocoaLumberjack"],
-                    path: "Tests",
-                    exclude: [],
-                    sources: ["Tests"])
+                exclude: ["Supporting Files"]),
+        
+//                sources: ["Classes"],
+//                publicHeadersPath: "Classes/Include"),
+        .target(name: "CocoaLumberjackSwift",
+                dependencies: ["CocoaLumberjack"],
+                exclude: ["Supporting Files"]),
+//        .testTarget(name: "Tests",
+//                    dependencies: ["CocoaLumberjack"],
+//                    path: "Tests",
+//                    exclude: [],
+//                    sources: ["Tests"])
     ]
 )
