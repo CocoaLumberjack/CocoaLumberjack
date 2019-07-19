f7f03c038db990c7c01d8bf3d97c6169d5626397
Dmitry Lobanov <gaussblurinc@gmail.com>
2019-07-19 11:43:04 +0300

    core: public header import has been fixed.

--- Sources/CocoaLumberjack/include/DDTTYLogger.h
+++ Sources/CocoaLumberjack/include/DDTTYLogger.h
@@ -31,7 +31,7 @@
     static inline DDColor* DDMakeColor(CGFloat r, CGFloat g, CGFloat b) {return [DDColor colorWithRed:(r/255.0f) green:(g/255.0f) blue:(b/255.0f) alpha:1.0f];}
 #elif defined(DD_CLI) || !__has_include(<AppKit/NSColor.h>)
     // OS X CLI
-    #import "CLIColor.h"
+    #import <CocoaLumberjack/CLIColor.h>
     typedef CLIColor DDColor;
     static inline DDColor* DDMakeColor(CGFloat r, CGFloat g, CGFloat b) {return [DDColor colorWithCalibratedRed:(r/255.0f) green:(g/255.0f) blue:(b/255.0f) alpha:1.0f];}
 #else
@@ -147,7 +147,7 @@
  * static NSString *const PurpleTag = @"PurpleTag";
  *
  * #define DDLogPurple(frmt, ...) LOG_OBJC_TAG_MACRO(NO, 0, 0, 0, PurpleTag, frmt, ##__VA_ARGS__)
- * 
+ *
  * And then where you configure CocoaLumberjack:
  *
  * purple = DDMakeColor((64/255.0), (0/255.0), (128/255.0));
