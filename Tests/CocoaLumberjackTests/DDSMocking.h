a3a940f3e24f7442bfffd865c2cf7422a208ca8f
Florian Friedrich <ffried@me.com>
2021-03-02 10:58:09 +0100

    Update Copyright to 2021

--- Tests/CocoaLumberjackTests/DDSMocking.h
+++ Tests/CocoaLumberjackTests/DDSMocking.h
@@ -1,6 +1,6 @@
 // Software License Agreement (BSD License)
 //
-// Copyright (c) 2010-2020, Deusty, LLC
+// Copyright (c) 2010-2021, Deusty, LLC
 // All rights reserved.
 //
 // Redistribution and use of this software in source and binary forms,
