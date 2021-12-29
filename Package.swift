3ae5c0baec18643e9fd3faf782135ec54c17aa09
Ricardo S Alba <risalba@gmail.com>
2021-12-29 09:53:12 +0100

    Make products dynamic

--- Package.swift
+++ Package.swift
@@ -15,12 +15,15 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CocoaLumberjack",
+            type: .dynamic,
             targets: ["CocoaLumberjack"]),
         .library(
             name: "CocoaLumberjackSwift",
+            type: .dynamic,
             targets: ["CocoaLumberjackSwift"]),
         .library(
             name: "CocoaLumberjackSwiftLogBackend",
+            type: .dynamic,
             targets: ["CocoaLumberjackSwiftLogBackend"]),
     ],
     dependencies: [
