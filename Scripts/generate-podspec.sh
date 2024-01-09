d2ada8c1dceb81a855439c02b0fbfeaf623211e9
Florian Friedrich <ffried@me.com>
2024-01-09 16:47:03 +0100

    Add privacy manifest to podspec generation

--- Scripts/generate-podspec.sh
+++ Scripts/generate-podspec.sh
@@ -145,6 +145,7 @@ Pod::Spec.new do |s|
   s.subspec 'Core' do |ss|
     ss.source_files         = 'Sources/CocoaLumberjack/**/*.{h,m}'
     ss.private_header_files = 'Sources/CocoaLumberjack/DD*Internal.{h}'
+    ss.resources            = 'Sources/CocoaLumberjack/PrivacyInfo.xcprivacy'
   end
 
   s.subspec 'Swift' do |ss|
