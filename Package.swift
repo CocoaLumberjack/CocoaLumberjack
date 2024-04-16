5c21c966de5bb221efc17ac897da8b41c9c0ade6
Florian Friedrich <ffried@me.com>
2024-04-16 16:18:09 +0200

    Drop Swift pre 5.8 support, add support for Swift 5.9 and 5.10, bump deployment targets to supported minimums

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.8
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,9 +7,9 @@ let package = Package(
     name: "CocoaLumberjack",
     platforms: [
         .macOS(.v10_13),
-        .iOS(.v11),
-        .tvOS(.v11),
-        .watchOS(.v4),
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v5),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
