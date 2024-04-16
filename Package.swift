a6977c996f703940de88156b3e813f85d5dbe316
Florian Friedrich <ffried@me.com>
2024-04-16 16:14:55 +0200

    Add visionOS support. Fixes #1415

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         .iOS(.v12),
         .tvOS(.v12),
         .watchOS(.v5),
+        .visionOS(.v1),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
