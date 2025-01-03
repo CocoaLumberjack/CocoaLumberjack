4986a03eb151a4072980ea9aed41bcd624173b5f
ffried <ffried@noreply.github.com>
2025-01-03 13:45:08 +0000

    Update copyright from 2024 to 2025

--- Sources/CocoaLumberjack/include/CocoaLumberjack/DDLogMacros.h
+++ Sources/CocoaLumberjack/include/CocoaLumberjack/DDLogMacros.h
@@ -1,6 +1,6 @@
 // Software License Agreement (BSD License)
 //
-// Copyright (c) 2010-2024, Deusty, LLC
+// Copyright (c) 2010-2025, Deusty, LLC
 // All rights reserved.
 //
 // Redistribution and use of this software in source and binary forms,
