55e94080b76ca1b7810edbc2be23f8df8771157b
Sergii Kovalenko <skovalenko@teamworkcommerce.com>
2025-03-18 11:53:03 +0200

    Fix swift-log url

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -25,7 +25,7 @@ let package = Package(
             targets: ["CocoaLumberjackSwiftLogBackend"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log", from: "1.5.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
